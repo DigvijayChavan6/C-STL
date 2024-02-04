9da7b99e3a053eca23ab64f28f01ba74f15f6cff
Digvijay Chavan <chavandigvijay9081@gmail.com>
2024-02-05 05:12:02 +0530

    rename

--- map/map.h
+++ map/map.h
@@ -3,17 +3,17 @@
 
 struct map_node{
     void *key , *value;
-    struct map_node *next;
+    struct map_node *left,*right;
 };
 
 typedef struct{
-    struct map_node *head;
+    struct map_node *root;
     size_t size;
     char *type;
 }map;
 
 map* map_create(char*,char*);
-void map_push(map*,void*,void*);
+void map_insert(map*,void*,void*);
 size_t map_size(map*);
 short map_isempty(map*);
 void map_clean(map*);
